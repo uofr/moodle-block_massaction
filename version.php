bc2ba673ca17ca98fa4746bdc101aa03dadc15ea
Matt Davidson <davidso1@rose-hulman.edu>
2022-03-18 08:54:39 -0400

    back to dev staging

--- version.php
+++ version.php
@@ -25,7 +25,7 @@
 defined('MOODLE_INTERNAL') || die;
 
 $plugin->version = 2022031500;
-$plugin->requires  = 2020061500;
+$plugin->requires  = 2021052500;
 $plugin->component = 'block_massaction';
-$plugin->maturity = MATURITY_STABLE;
-$plugin->release = 'v6.0.0';
+$plugin->maturity = MATURITY_ALPHA;
+$plugin->release = 'v7.0.0';
